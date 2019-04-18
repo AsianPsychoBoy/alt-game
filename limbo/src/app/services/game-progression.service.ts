2b620c4f29e12171054013b914fbac2bf1d26b98
AsianPsychoBoy <ziyang_99@outlook.com>
2019-04-17 19:20:56 -0500

    fix level stuff

--- limbo/src/app/services/game-progression.service.ts
+++ limbo/src/app/services/game-progression.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class GameProgressionService {
 
-  currentLevel = -1;
+  currentLevel = 0;
 
   currentItems: [];
 
@@ -31,33 +31,33 @@ export class GameProgressionService {
   }
 
   gotoLevel(n: number): Observable<boolean> {
-	return new Observable((subscriber) => {
-		const level = levels.find(lvl => lvl.number === n);
-		if (level && level.requirement.level === this.currentLevel) {
-			this.currentLevel = level.number;
-			subscriber.next(true);
-			subscriber.complete();
-		} else {
-			subscriber.next(false);
-			subscriber.complete();
-		}
-	});
+    return new Observable((subscriber) => {
+      const level = levels.find(lvl => lvl.number === n);
+      if (level && level.requirement.level === (this.currentLevel - this.currentLevel % 1)) {
+        this.currentLevel = level.number;
+        subscriber.next(true);
+        subscriber.complete();
+      } else {
+        subscriber.next(false);
+        subscriber.complete();
+      }
+    });
   }
 
 }
 
 const levels = [
   {
-    number: 0,
+    number: 1,
     requirement: {
-		level: -1,
+		  level: 0,
       items: []
     }
   },
   {
-    number: 1,
+    number: 1.1,
     requirement: {
-      level: 0,
+      level: 1,
       command: [2, 3],
       items: []
     }
@@ -67,6 +67,30 @@ const levels = [
     requirement: {
       level: 1,
       command: [],
+      items: []
+    }
+  },
+  {
+    number: 2.1,
+    requirement: {
+      level: 2,
+      command: [6, 5],
+      items: []
+    }
+  },
+  {
+    number: 2.2,
+    requirement: {
+      level: 2,
+      command: [2, 4],
+      items: []
+    }
+  },
+  {
+    number: 2.3,
+    requirement: {
+      level: 2,
+      command: [2, 4],
       items: []
     }
   }
